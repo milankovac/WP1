e083c93d53912da69209cf748a655696eec8bd87
Forwardslash NY <milankovac75@gmail.com>
2021-03-04 11:39:37 +0100

    Finisheed Validation

--- wp-content/themes/storefront/Registration.php
+++ wp-content/themes/storefront/Registration.php
@@ -16,7 +16,8 @@ class Registration {
 
 		add_filter( 'acf/validate_value/name=phone_number', array( $this, 'validate_phone_number' ), 10, 4 );
 		add_filter( 'acf/validate_value/name=birth_year', array( $this, 'validate_birth_year' ), 10, 4 );
-
+		add_action( 'woocommerce_edit_account_form', array( $this, 'acf_add_field_on_edit_account' ) );
+		add_action( 'woocommerce_save_account_details_errors', array( $this,'acf_field_edit_account_errors_and_update' ), 10, 1 );
 	}
 
 	/**
@@ -100,5 +101,49 @@ class Registration {
 		}
 	}
 
+	/**
+	 *Add fields in the profile details section
+	 */
 
+	public function acf_add_field_on_edit_account() {
+		?>
+        <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+            <label for="password_2"><?php esc_html_e( 'Phone Number', 'woocommerce' ); ?></label>
+            <input type="tel" class="woocommerce-Input woocommerce-Input--password input-text" name="number" id="number"
+                   autocomplete="off" value="<?php echo get_field('phone_number',wp_get_current_user()) ?>"/>
+        </p>
+        <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+            <label for="password_2"><?php esc_html_e( 'Date of Birth', 'woocommerce' ); ?></label>
+            <input type="number" class="woocommerce-Input woocommerce-Input--password input-text"
+                   name="birthday" id="birthday" autocomplete="off" value="<?php echo get_field('birth_year',wp_get_current_user()) ?>"/>
+        </p>
+		<?php
+
+	}
+
+	/**
+	 * If the entry is not valid, a message will be generated stating that if it is valid, the field will be updated
+	 *
+	 * @param $args
+	 */
+
+	public function acf_field_edit_account_errors_and_update( $args ) {
+
+		if ( isset( $_POST['number'] ) ) {
+			$number = $_POST['number'];
+			if ( $this->condition_for_phone_number($number) ) {
+				$args->add( 'error', __( 'The phone must have at least 4 numbers' ), '' );
+			} else {
+				update_user_meta( wp_get_current_user()->ID, 'phone_number', sanitize_text_field( $_POST['number'] ) );
+			}
+		}
+		if ( isset( $_POST['birthday'] ) ) {
+			$birth_year = $_POST['birthday'];
+			if ( $this->condition_for_birth_year($birth_year) ) {
+				$args->add( 'error', __( 'You must be over 18 and over 100 years old' ), '' );
+			} else {
+				update_user_meta( wp_get_current_user()->ID, 'birth_year', sanitize_text_field( $_POST['birthday'] ) );
+			}
+		}
+	}
 }
