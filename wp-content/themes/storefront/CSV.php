89a979229dab59e7ffa64f30880a7d8e82abbedc
Forwardslash NY <milankovac75@gmail.com>
2021-03-08 09:45:51 +0100

    CSV Finished

--- wp-content/themes/storefront/CSV.php
+++ wp-content/themes/storefront/CSV.php
@@ -5,67 +5,87 @@
  * It is intended for creating a CSV file
  * instantiates in functions.php line 100
  */
+
 class CSV {
+
+
 	/**
 	 * CSV constructor.
 	 */
+
 	public function __construct() {
-
-		add_action( 'woocommerce_after_add_to_cart_button', array( $this, 'on_woocommerce_before_add_to_cart_button' ) );
-header();
-		add_action( 'wp_ajax_get_data', array( $this, 'get_data_all' ) );
-		add_action( 'wp_ajax_nopriv_get_data', array( $this, 'get_data_all' ) );
-
-		var_dump($_POST['color']);
+		add_action( 'woocommerce_after_add_to_cart_button', array($this, 'on_woocommerce_before_add_to_cart_button' ));
+		add_action( 'init', array( $this, 'on_init' ) );
 	}
 
 	/**
-	 * when someone knocks a button "donwload" the download of the corresponding file will start
-	 * since the file does not exist, its creation will start first using the function create_csv() which returns the file name;
+	 * If the product is variable, a button will appear on that side
 	 */
+
 	public function on_woocommerce_before_add_to_cart_button() {
 		global $product;
-		if($product->is_type('variable'))
-		{
-			$site_url = get_template_directory_uri().'/'.$this->create_csv().'.csv';
-			echo "<br><br><a class='single_add_to_cart_button button alt' style='text-decoration: none;color: white' href='{$site_url}';>Download</a>";
-		}
-		else
-		{
-			echo'';
-		}
 
+		$product_id = $product->id;
+		if ( $product->is_type( 'variable' ) ) {
+			$url = get_permalink() . "?download={$product_id}&&name={$product->name}";
 
+			echo "<br><br><a  class='button alt' style='text-decoration: none;color: white' href='{$url}' ;>Download</a>";
+		} else {
+			echo '';
+		}
 	}
 
 	/**
-	 * Used to create and add data to a file
-	 * @return mixed
+	 * Generation  CSV file
+	 * Returns table values
+	 * @return string
 	 */
-	public function create_csv() {
-		global $product;
 
+	public function generate_csv() {
 
-//			if ( $product ) {
-//
-//				$file_name = $product->name;
-//				$path      = $_SERVER['DOCUMENT_ROOT'] . '/wp1/wp-content/themes/storefront/' . $file_name . '.csv';
-//				$file      = fopen( $path, 'w' );
-//				fputcsv( $file, array( 'post_id', 'description', 'price' ) );
-//				fputcsv( $file, array( $product->id, $_POST['pa_size'], $product->price ) );
-//				return $file_name;
-//			}
-	}
+		$product = wc_get_product( $_GET['download']);
+		$attribute_label_name='id,';
+		$all=array();
+		$all_value='';
+		foreach ($product->get_variation_attributes() as $taxonomy => $term_names ) {
+			$attribute_label_name .= wc_attribute_label($taxonomy).',';
 
-	public function get_data_all(){
-		$color=$_POST['color'];
-		if($_GET['donwload']){
-			$path      = $_SERVER['DOCUMENT_ROOT'] . '/wp1/wp-content/themes/storefront/app.csv';
-			$file      = fopen( $path, 'w' );
-			fputcsv( $file, array( 'post_id', $color, 'price' ) );
+		}
+		$attribute_label_name.='price';
+		$all[]=$attribute_label_name;
+
+		foreach ($product->get_available_variations() as $key=>$variation)
+		{
+
+			foreach ($variation['attributes'] as $value)
+			{
+				$all_value.=$value.',';
+			}
+			array_push($all,"{$variation['variation_id']},".$all_value."{$variation['display_price']}");
+			$all_value='';
 		}
 
-
+		$string_data=implode("\n",$all);
+		return $string_data;
 	}
 
+	/**
+	 *If someone clicks the download button, the file download will start
+	 */
+
+	public function on_init() {
+
+		if ( isset( $_GET['download'] ) ) {
+
+			$csv = $this->generate_csv();
+			header( "Content-Type: text/csv" );
+			header( "Content-Disposition: attachment; filename={$_GET['name']}.csv" );
+			header( "Cache-Control: no-cache, no-store, must-revalidate" );
+			header( "Pragma: no-cache" );
+			header( "Expires: 0" );
+			header( "Content-Transfer-Encoding: UTF-8" );
+			echo $csv;
+			exit;
+		}
+	}
 }
